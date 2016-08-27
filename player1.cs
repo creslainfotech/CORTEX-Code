aafab4f24c489d50c711063b9cfc00b892492ef6
bodhish <bodhish@gmail.com>
2016-08-27 22:09:18 +0530

    Player Codes
    Updated and initalised player

---
+++ player1.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class player1 : MonoBehaviour
+{
+   public float playerboundar = 1f;
+    Rigidbody2D mybody1;
+    public float moveforce = 1f; //force to push the player1 (mybody1)
+
+
+    // Player 
+    void Start()
+    {
+        mybody1 = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // 
+    void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+        Vector3 moveplayer1 = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"),0,0) * moveforce * Time.deltaTime;
+
+        pos += moveplayer1;
+        mybody1.AddForce(moveplayer1);
+
+        // Setting the border limits
+        float screenRatio = (float) Screen.width / (float) Screen.height;
+        float widthOrtho = Camera.main.orthographicSize * screenRatio;
+        
+        if(pos.x + playerboundar > widthOrtho)
+        {
+            pos.x = widthOrtho - playerboundar; 
+        }
+        if (pos.x - playerboundar < - widthOrtho)
+        {
+            pos.x = - widthOrtho + playerboundar;
+        }
+
+        transform.position = pos;
+    }
+
+  
+
+
+    }
---
+++ player2.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class player2 : MonoBehaviour
+{
+    public float playerboundar = 1;
+    Rigidbody2D mybody2;
+    public float moveforce = 1; //force to push the player1 (mybody1)
+
+
+    // Player 
+    void Start()
+    {
+        mybody2 = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // 
+    void FixedUpdate()
+    {
+              
+
+        Vector3 pos1 = transform.position;
+        Vector3 moveplayer2 = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal1"), 0, 0) * moveforce * Time.deltaTime;
+
+        pos1 += moveplayer2;
+        mybody2.AddForce(moveplayer2);
+
+        // Setting the border limits
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float widthOrtho = Camera.main.orthographicSize * screenRatio;
+
+        if (pos1.x + playerboundar > widthOrtho)
+        {
+            pos1.x = widthOrtho - playerboundar;
+        }
+        if (pos1.x - playerboundar < -widthOrtho)
+        {
+            pos1.x = -widthOrtho + playerboundar;
+        }
+
+        transform.position = pos1;
+
+
+    }
+}
