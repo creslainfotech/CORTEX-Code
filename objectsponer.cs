f60f63ea8fdc814a8353cd78b6453bbca75634c9
bodhish <bodhish@gmail.com>
2016-08-28 16:58:33 +0530

    Obstucle Sponner
    Obstucle sponer

---
+++ objectsponer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectsponer : MonoBehaviour {
+
+    public GameObject[] ob;
+    public Transform campos;
+    
+	// Use this for initialization
+	void Start () {
+        ObstacleMaker();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        transform.Translate(Vector3.down * PlayerPrefs.GetFloat("speed") * Time.deltaTime );
+               
+       
+	}
+
+
+
+
+    void ObstacleMaker()
+    {
+        GameObject clone = (GameObject)Instantiate (ob[Random.Range(0,ob.Length)], transform.position, Quaternion.identity);
+        clone.name = "Quard"; 
+        clone.AddComponent<BoxCollider2D>();
+        clone.GetComponent<BoxCollider2D>().isTrigger = true;
+        float xx = Random.Range(3, 4);
+        Invoke("ObstacleMaker", xx); 
+    }
+
+}
+
